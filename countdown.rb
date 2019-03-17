9d67972216ad3b15ec9ec2fc433e6a60da3f77e5
Dhara Patel <1991dharapatel@gmail.com>
2019-03-17 14:37:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number_of_sec)
-  number_of_sec=0 
-    while number_of_sec>n
-      return  "HAPPY NEW YEAR!"
-  number_of_sec+=1
+  x=1 
+    while x<number_of_sec
+  return  "HAPPY NEW YEAR!"
+  x+=1
 end
 end
